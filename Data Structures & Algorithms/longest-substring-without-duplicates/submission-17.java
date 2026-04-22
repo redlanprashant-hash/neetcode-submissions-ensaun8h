0b6fbbac1d28db01ea47cb076a0dc39b73d448a6
redlanprashant-hash <redlanprashant@gmail.com>
2026-04-22 13:05:54 +0530

    Add: longest-substring-without-duplicates - submission-17

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-17.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+
+        int len = s.length();
+        if(len == 0){
+            return 0;
+        }
+        if(len == 1){
+            return 1;
+        }
+
+        int left = 0;
+        int right = 0;
+        int ans = 0;
+        HashSet<Character> hs = new HashSet<>();
+        while(right < len){
+            char ch = s.charAt(right);
+            while(hs.contains(ch)){
+                hs.remove(s.charAt(left));
+                left++;
+            }
+            hs.add(ch);
+            ans = Math.max(ans, right - left + 1);
+            right++;
+        }
+        return ans;
+
+
+    }
+}
