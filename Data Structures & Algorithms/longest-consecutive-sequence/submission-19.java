1093664ad3027d8609cd915930339afd7b3d71ac
redlanprashant-hash <redlanprashant@gmail.com>
2026-04-27 11:57:24 +0530

    Add: longest-consecutive-sequence - submission-19

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-19.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        int longestSub = 1;
+        HashSet<Integer> hs = new HashSet<>();
+        for(int num : nums){
+            hs.add(num);
+        }
+
+        for(int num : hs){
+            if(hs.contains(num - 1)){
+                continue;
+            }else{
+                int currentSub = 1;
+                int currentNum = num;
+                while(hs.contains(currentNum + 1)){
+                    currentNum++;
+                    currentSub++;
+                }
+                longestSub = Math.max(longestSub,currentSub);
+            }
+        }
+    return longestSub;
+
+
+
+    }
+}
