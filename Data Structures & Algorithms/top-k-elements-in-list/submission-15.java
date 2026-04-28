ffd858d455969e48fad1b535bbe75ee1daf37a07
redlanprashant-hash <redlanprashant@gmail.com>
2026-04-28 11:49:38 +0530

    Add: top-k-elements-in-list - submission-15

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-15.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {    
+    
+        HashMap<Integer,Integer> freqMap = new HashMap<>();
+        for(int num : nums){
+            freqMap.put(num, freqMap.getOrDefault(num,0)+1);
+        }
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>(
+            (a,b) -> freqMap.get(a) - freqMap.get(b)
+        );
+
+        for(int num : freqMap.keySet()){
+            
+            heap.add(num);
+            if(heap.size() > k){
+                heap.poll();
+            }
+
+        }
+
+        int[] arr = new int[k];
+        for(int i = 0; i < k; i++){
+            arr[i] = heap.poll();
+        }
+        return arr;
+
+    }
+}
