86ac9c39e4ae43b6808e1a2c3c2a88e6e5da8023
redlanprashant-hash <redlanprashant@gmail.com>
2026-04-28 12:21:13 +0530

    Add: valid-sudoku - submission-8

---
+++ Data Structures & Algorithms/valid-sudoku/submission-8.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int N = 9;
+        HashSet<Character>[] rows = new HashSet[N];
+        HashSet<Character>[] cols = new HashSet[N];
+        HashSet<Character>[] boxes = new HashSet[N];
+
+        for(int i = 0; i < N; i++){
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+
+        for(int row = 0; row < N; row++){
+            for(int col = 0; col < N; col++){
+                char val = board[row][col];
+                if(val == '.'){
+                    continue;
+                }
+
+                if(rows[row].contains(val)){
+                    return false;
+                }
+                rows[row].add(val);
+
+                if(cols[col].contains(val)){
+                    return false;
+                }
+                cols[col].add(val);
+
+                int index = (row/3) * 3 + (col/3);
+                if(boxes[index].contains(val)){
+                    return false;
+                }
+                boxes[index].add(val);
+            }
+        }
+        return true;
+
+    }
+}
