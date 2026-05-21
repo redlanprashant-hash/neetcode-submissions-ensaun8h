66cbb0cfe75bb37789999429a77efe93fed6f10b
redlanprashant-hash <redlanprashant@gmail.com>
2026-05-21 11:47:35 +0530

    Add: anagram-groups - submission-14

---
+++ Data Structures & Algorithms/anagram-groups/submission-14.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> ansMap = new HashMap<>();
+        int[] count = new int[26];
+        for(String str : strs){
+            Arrays.fill(count,0);
+            for(char ch : str.toCharArray()){
+                count[ch - 'a']++;
+            }
+
+            StringBuffer sb = new StringBuffer();
+            for(int i = 0; i < 26; i++){
+                sb.append(count[i]);
+                sb.append("#");
+            }
+
+            String key = sb.toString();
+            if(!ansMap.containsKey(key)){
+                ansMap.put(key,new ArrayList());
+            }
+            ansMap.get(key).add(str);
+
+        }
+
+        return new ArrayList(ansMap.values());
+    }
+}
